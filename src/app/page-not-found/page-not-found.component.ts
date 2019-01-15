713277d58ad9c3fbb146f1ae7f9814b761e0a076
Michael Upton <uptonm@wit.edu>
2019-01-15 08:33:54 -0500

    Static data in route determines error page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
+  errorCode: string;
+  errorMessage: string;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.errorCode = this.route.snapshot.data['errorCode'];
+    this.errorMessage = this.route.snapshot.data['errorMessage'];
+    this.route.data.subscribe((data: Data) => {
+      this.errorCode = data['errorCode'];
+      this.errorMessage = data['errorMessage'];
+    });
   }
-
 }
