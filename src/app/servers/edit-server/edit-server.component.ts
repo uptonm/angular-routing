b30cb00923cf9488443d9a1d814e3ab791d9b681
Michael Upton <uptonm@wit.edu>
2019-01-15 08:34:11 -0500

    Added catch for leaving edit-server route before changes are saved

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ServersService } from '../servers.service';
 import { Router, NavigationEnd, ActivatedRoute, Params } from '@angular/router';
+import { CanComponentDeactivate } from './canDeactivate.guard';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit, OnDestroy {
+export class EditServerComponent
+  implements OnInit, OnDestroy, CanComponentDeactivate {
   server: { id: number; name: string; status: string } = {
     id: 0,
     name: 'Loading',
@@ -16,6 +19,9 @@ export class EditServerComponent implements OnInit, OnDestroy {
   sub: any;
   subQuery: any;
   allowEdit: Boolean = false;
+  changesSaved: Boolean = false;
+  serverName: string;
+  serverStatus: string;
 
   constructor(
     private serversService: ServersService,
@@ -28,7 +34,6 @@ export class EditServerComponent implements OnInit, OnDestroy {
     this.allowEdit = +this.route.snapshot.queryParams['allowEdit'] === 1;
     console.log(this.allowEdit);
     this.server = this.serversService.getServer(id);
-
     this.sub = this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
     });
@@ -40,13 +45,30 @@ export class EditServerComponent implements OnInit, OnDestroy {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {
-      name: this.server.name,
-      status: this.server.status
+      name: this.serverName,
+      status: this.serverStatus
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
     this.subQuery.unsubscribe();
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if (
+      (this.server.name !== this.serverName ||
+        this.server.status !== this.serverStatus) &&
+      !this.changesSaved
+    ) {
+      return confirm('Are you sure you want to leave without saving changes?');
+    } else {
+      return true;
+    }
   }
 }
